64574369cc90e7892fa88559bd1da9b71b4781aa
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2013-09-23 22:51:45 -0500

    hace la graficación

--- main.py
+++ main.py
@@ -5,9 +5,9 @@
 % humps(x) on [0,3]
 '''
 from numpy import linspace
-from pwLEval import*
-from pylab import plot, figure, title
-from pwL import *
+from modulos.pwLEval import*
+import matplotlib.pyplot as plt
+from modulos.pwL import *
 
 def humps(x):
      y=(1.0/((x-.3)**2)+.01)+((1.0/(  ((x-.9)**2) +.04)))-6
@@ -17,11 +17,12 @@ def humps(x):
 z = linspace(0,3,200)
 fvals = humps(z)
 for n in [5, 10, 25, 50]:
-   figure()
+   f = plt.figure()
    x = linspace(0,3,n)
    y = humps(x)
    a, b = pwL(x,y)
    Lvals = pwLEval(a,b,x,z)
 
-   plot(z,Lvals,z,fvals,'--',x,y,'o')
-   title('Interpolation of humps(x) with pwL, n = %2.0f'%n)
+   plt.plot(z,Lvals,z,fvals,'--',x,y,'o')
+   plt.title('Interpolation of humps(x) with pwL, n = %2.0f'%n)
+   plt.show()
