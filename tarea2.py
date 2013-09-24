51e2e7b299f61a61f694fc1b226ba5cb7fb984ae
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2013-09-23 23:36:58 -0500

    dejamos el módulo funcional

--- tarea2.py
+++ tarea2.py
@@ -7,17 +7,12 @@ import time
 x=linspace(-1,1,100)
 y=1.0/(1.0+25*(x**2))
 
-def update_line(num, data, l1, l2, l3):
-    pass
-
-#for i in range(7,16,2):
-fig = plt.figure()
-xequal=linspace(-1,1,i)
-yequal=1.0/(1.0+25*(xequal**2))
-a=coeffts(xequal,yequal)
-pval=evalPoly(a,xequal,x)
-l1, l2, l3 = plt.plot(x,y,x,pval,xequal,yequal,'*')
-line_ani = animation.FuncAnimation(fig1, update_line, 25, fargs=(data, l1, l2, l3), interval=50, blit=True)
-plt.show(block=False)
-time.sleep(1)
-#endfor
+for i in range(7,16,2):
+    fig = plt.figure()
+    xequal=linspace(-1,1,i)
+    yequal=1.0/(1.0+25*(xequal**2))
+    a=coeffts(xequal,yequal)
+    pval=evalPoly(a,xequal,x)
+    l1, l2, l3 = plt.plot(x,y,x,pval,xequal,yequal,'*')
+    plt.show(block=False)
+    time.sleep(1)
