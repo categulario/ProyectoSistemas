3b1ca7595822d389afbd79c39ba493dc993508a8
Abraham Toriz Cruz <a.wonderful.code@gmail.com>
2013-09-23 23:34:47 -0500

    intentando integrar la animación

--- tarea2.py
+++ tarea2.py
@@ -1,17 +1,22 @@
 import matplotlib.pyplot as plt
 from numpy import ones, linspace
-#from newtonPoly import *
+from modulos.newtonPoly import *
+import matplotlib.animation as animation
 import time
 
 x=linspace(-1,1,100)
 y=1.0/(1.0+25*(x**2))
-for i in range(7,16,2):
-    fig = plt.figure()
-    xequal=linspace(-1,1,i)
-    yequal=1.0/(1.0+25*(xequal**2))
-    #a=coeffts(xequal,yequal)
-    #pval=evalPoly(a,xequal,x)
-    #plt.plot(x,y,x,pval,xequal,yequal,'*')
-    plt.plot(x,y,'*')
-    plt.show(block=False)
-    time.sleep(3)
+
+def update_line()
+
+#for i in range(7,16,2):
+fig = plt.figure()
+xequal=linspace(-1,1,i)
+yequal=1.0/(1.0+25*(xequal**2))
+a=coeffts(xequal,yequal)
+pval=evalPoly(a,xequal,x)
+l1, l2, l3 = plt.plot(x,y,x,pval,xequal,yequal,'*')
+line_ani = animation.FuncAnimation(fig1, update_line, 25, fargs=(data, l1, l2, l3), interval=50, blit=True)
+plt.show(block=False)
+time.sleep(1)
+#endfor
